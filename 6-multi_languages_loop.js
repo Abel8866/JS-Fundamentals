a04f5fdd16b86a8741bf009470ccd34c15659184
Abel8866 <abel.endale-ug@aau.edu.et>
2025-11-08 13:50:03 +0300

    content corrected here

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 const x = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-for (int i = 0; i < x.length; i++) {
-  console.log(x[i]);
+let out = "";
+for (let i = 0; i < x.length; i++) {
+    out += x[i] + "\n";
 }
+console.log(out.trim());
